89ef1161abb8c5e8c7177402483dd910df741f47
rrahul1011 <68853654+rrahul1011@users.noreply.github.com>
2023-08-04 16:36:42 +0530

    Stock price

--- streamlit_exp.py
+++ streamlit_exp.py
@@ -2,5 +2,29 @@ import numpy as np
 import pandas as pd
 import seaborn as sns 
 import streamlit as st
+import yfinance as yf
 
-st.header("This is my first project in streamlit")
+st.write("""
+         ## Simple Stock Price App
+         
+         Shown are the stock closing price and volume of Google!
+
+         """)
+tickersymbol = 'AAPL'
+
+tickerData =yf.Ticker(tickersymbol)
+
+tickerdf =  tickerData.history(period="1d", start="2020-01-01",end='2023-08-01')
+st.write("""
+### Closing Price
+""")
+st.line_chart(tickerdf["Close"])
+
+st.write("""
+### Total Volume
+""")
+st.line_chart(tickerdf["Volume"])
+st.write(tickerdf)
+
+
+
